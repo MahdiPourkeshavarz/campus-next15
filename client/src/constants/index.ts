94fbc52fb59b70823535d196fc57414714323059
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-02-14 19:19:33 +0330

    feat(home page data): creating the structure and content for home page data in strapi

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,3 +1,6 @@
 export const BASE_URL: string = "http://localhost:1337";
 
 export const HOME_URL: string = "/api/home-page";
+
+export const HOME_PAGE_DATA: string =
+  "api/home-page?populate[blocks][on][blocks.hero-section][populate][image][fields][0]=url&populate[blocks][on][blocks.hero-section][populate][image][fields][1]=alternativeText&populate[blocks][on][blocks.hero-section][populate][logo][populate][image][fields][0]=url&populate[blocks][on][blocks.hero-section][populate][logo][populate][image][fields][1]=alternativeText&populate[blocks][on][blocks.hero-section][populate][cta]=true&populate[blocks][on][blocks.info-block][populate][image][fields][0]=url&populate[blocks][on][blocks.info-block][populate][image][fields][1]=alternativeText&populate[blocks][on][blocks.info-block][populate][cta]=true";
